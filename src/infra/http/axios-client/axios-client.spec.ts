a5264000e1da7a6645d349ac7aaa13c4b46f49a7
Gabriel Júlio <gabrieljuliobs@gmail.com>
2022-11-06 08:47:43 -0300

    Adicionado AxiosClient

---
+++ src/infra/http/axios-client/axios-client.spec.ts
@@ -0,0 +1,36 @@
+import { AxiosClient } from './axios-client'
+import { mockAxiosClient } from '@/infra/test/mock'
+import { mockPostRequest } from '@/data/test/mock'
+import axios from 'axios'
+
+jest.mock('axios')
+
+type SutTypes = {
+  sut: AxiosClient
+  mockedAxios: jest.Mocked<typeof axios>
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosClient()
+  const mockedAxios = mockAxiosClient()
+
+  return {
+    sut,
+    mockedAxios
+  }
+}
+
+describe('AxiosClient', () => {
+  test('Should call axios with correct values', async () => {
+    const request = mockPostRequest()
+    const { sut, mockedAxios } = makeSut()
+    await sut.post(request)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
+
+  test('Should return correct statusCode and body', () => {
+    const { sut, mockedAxios } = makeSut()
+    const promise = sut.post(mockPostRequest())
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
+  })
+})
---
+++ src/infra/http/axios-client/axios-client.ts
@@ -0,0 +1,12 @@
+import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
+import axios from 'axios'
+
+export class AxiosClient implements HttpPostClient<any, any> {
+  async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const response = await axios.post(params.url, params.body)
+    return {
+      statusCode: response.status,
+      body: response.data
+    }
+  }
+}
